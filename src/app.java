e7a044319d47b0f113fd444ea10d44b2aaba5fdf
Fedia23 <fedia.ch@gmail.com>
2019-05-07 22:00:46 +0300

    add scanner

--- src/app.java
+++ src/app.java
@@ -1,19 +1,21 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class app {
     public static void main(String[] args) {
 
         List<Integer> list = new LinkedList<>();
-        list.add(4);
-        list.add(1);
-        list.add(3);
-        list.add(14);
-        list.add(44);
-        list.add(42);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter five numbers :");
+        for (int i = 0; i <= 5; i++) {
+            list.add(scanner.nextInt());
+        }
+
         InsertionSort insertionSort = new InsertionSort(list);
         insertionSort.sortNumberDescending();
 
+        System.out.println("Result of sorting :");
         list.stream().forEach(s -> System.out.println(s));
     }
 }
